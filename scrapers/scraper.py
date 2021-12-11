68f99f8b89fabe210dd6f5fb43cdd65e708c472c
kropidlowsky <kropidlowsky@protonmail.com>
2021-12-11 21:06:59 +0100

    update scraper documentation

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -4,16 +4,28 @@ from bs4 import BeautifulSoup
 
 
 class Scraper(abc.ABC):
+    """A class used to represent a custom web scraper"""
+
     def __init__(self, url: str):
+        """
+        :param url: url to scrape
+        """
         self.url: str = url
         self._soup: BeautifulSoup = helper.get_soup(helper.get_response(self.url))
 
     @abc.abstractmethod
-    # scrape website to collect data
     def _scrape(self) -> None:
+        """
+        scrape website to collect data
+        :return: None
+        """
         pass
 
     @abc.abstractmethod
-    # prepare desired result
     def _prepare_result(self, data) -> None:
+        """
+        prepare desired result
+        :param data: bs4 data to process
+        :return: None
+        """
         pass
