a1937562014b4cc77f0dae0b3f049777004f9c2f
kropidlowsky <kropidlowsky@protonmail.com>
2021-12-11 20:56:03 +0100

    rename representative_tuple to representative_list

--- scrapers/representative_list.py
+++ scrapers/representative_list.py
@@ -1,5 +1,4 @@
 from scraper import Scraper
-from bs4 import BeautifulSoup
 import bs4
 
 
@@ -8,26 +7,27 @@ def _extract_a(elements: bs4.element.ResultSet) -> list:
     return [e.select_one('a') for e in elements]
 
 
-class RepresentativeTuple(Scraper):
+class RepresentativeList(Scraper):
 
     def __init__(self, url: str = "https://www.sejm.gov.pl/Sejm9.nsf/poslowie.xsp?type=A"):
         super().__init__(url)
         self._scrape()
 
-    def _scrape(self):
+    def _scrape(self) -> None:
         uls = self.__get_deputies_uls()
         representative_hyperlinks = _extract_a(uls)
-        self.__make_tuple(representative_hyperlinks)
+        self._prepare_result(representative_hyperlinks)
 
     # get list of ul (HTML unordered list) elements containing deputies (representatives) class
     def __get_deputies_uls(self) -> bs4.element.ResultSet:
         return self._soup.select('ul.deputies')
 
-    def __make_tuple(self, hyperlinks: list) -> None:
-        self.generator = (a['href'] for a in hyperlinks)
+    # get result (generator) with deputies (representatives) hyperlinks
+    def _prepare_result(self, data) -> None:
+        self.result = (a['href'] for a in data)
 
 
 if __name__ == '__main__':
-    rl = RepresentativeTuple()
-    for r in rl.generator:
+    rl = RepresentativeList()
+    for r in rl.result:
         print(r)
