4da3fc76e610e9036a8df06f19d523c1ffb5d8c4
kropidlowsky <kropidlowsky@protonmail.com>
2021-12-11 20:56:59 +0100

    improve scraper abstract class

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,12 +1,19 @@
 import helper
 import abc
+from bs4 import BeautifulSoup
 
 
 class Scraper(abc.ABC):
     def __init__(self, url: str):
-        self.url = url
-        self._soup = helper.get_soup(helper.get_response(self.url))
+        self.url: str = url
+        self._soup: BeautifulSoup = helper.get_soup(helper.get_response(self.url))
 
     @abc.abstractmethod
-    def _scrape(self):
+    # scrape website to collect data
+    def _scrape(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    # prepare desired result
+    def _prepare_result(self, data) -> None:
         pass
