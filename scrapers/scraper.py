2d08e6e505f7067641d3e2955ac5ca62d8532c85
kropidlowsky <kropidlowsky@protonmail.com>
2021-12-11 21:15:58 +0100

    Update scraper.py

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 class Scraper(abc.ABC):
-    """A class used to represent a custom web scraper"""
+    """An abstract class used to represent a custom web scraper"""
 
     def __init__(self, url: str):
         """
