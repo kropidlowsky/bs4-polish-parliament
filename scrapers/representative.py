d8eab4a89526312f5d0aca2de2a1f6f51e07dba3
kropidlowsky <kropidlowsky@protonmail.com>
2021-12-19 21:42:17 +0100

    add __get_email method

--- scrapers/representative.py
+++ scrapers/representative.py
@@ -1,5 +1,7 @@
 from scraper import Scraper
 import bs4
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class Representative(Scraper):
@@ -7,19 +9,25 @@ class Representative(Scraper):
     Scraper class to represent a representative (deputy) profile.
     """
 
-    def __init__(self, url: str):
+    def __init__(self, url: str, get_dynamic: bool = False):
         """
         :param url: representative (deputy) profile url
         """
         super().__init__(url)
+        self._get_dynamic = get_dynamic
+        self.result = dict()
         self._scrape()
-        self._prepare_result(None)
 
     def _scrape(self) -> None:
         """
         Scrape representative (deputy) profile url.
         """
         self.__info_div = self.__get_info_div()
+        self.result['name'] = self.__get_name()
+        self.result['picture'] = self.__get_picture()
+        data_uls = self.__get_data_uls()
+        self.__get_static_info(data_uls)
+        self.__get_email()
 
     def __get_info_div(self) -> bs4.BeautifulSoup:
         """
@@ -27,18 +35,6 @@ class Representative(Scraper):
         :return: information div
         """
         return self._soup.select_one('div', {'id': 'title_content'})
-
-    def _prepare_result(self, data) -> None:
-        """
-        Prepare result dict with representative (deputy) information
-        :param data: None
-        """
-        data_uls = self.__get_data_uls()
-        self.result = {
-            'name': self.__get_name(),
-            'picture': self.__get_picture()
-        }
-        self.__get_static_info(data_uls)
 
     def __get_name(self) -> str:
         return self.__info_div.select_one('H1').get_text()
@@ -49,19 +45,24 @@ class Representative(Scraper):
     def __get_data_uls(self) -> bs4.element.ResultSet:
         return self.__info_div.select('ul.data')
 
-    def __get_static_info(self, elements: bs4.element.ResultSet):
+    def __get_static_info(self, uls: bs4.element.ResultSet):
         """
         Get information from static HTML elements.
-        :param elements: HTML elements to loop through
+        :param uls: HTML elements to loop through
         """
-        for i, element in enumerate(elements):
-            # avoid dynamic elements
-            if i > 1:
-                break
-            for li in element.select('li'):
-                key = li.select_one('p.left').get_text()
-                if key:
-                    self.result[key] = li.select_one('p.right').get_text()
+        for li in uls[0].select('li'):
+            key = li.select_one('p.left').get_text()
+            if key:
+                self.result[key] = li.select_one('p.right').get_text()
+
+    # def __get_dynamic_info(self):
+    #     pass
+
+    def __get_email(self):
+        """
+        Get email address from href value (must be parsed) to avoid using Selenium.
+        """
+        self.result['email'] = self.__info_div.select_one('#view\:_id1\:_id2\:facetMain\:_id191\:_id280').get('href')
 
 
 if __name__ == '__main__':
